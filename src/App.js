62dfff3d37cd8dd9f30f23e13a9789664a61467b
Isgramor <maksaj9@gmail.com>
2020-07-03 04:59:53 +0400

    the first changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="" style={{backgroundImage: "url(./img/wallpaper.jpg)", width: "100vw", height: "100vh",position:"absolute",top:"0px",left:"0px"}}>
+      <h1>Hi</h1>
     </div>
+    
   );
+  
 }
 
 export default App;
